8aebe1259bd5118e049dc0e0c08c2ce929c050f4
Imran <89724435+Huseynlimran@users.noreply.github.com>
2021-12-19 13:33:38 +0400

    Add files via upload

---
+++ Room.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal('.top',{
+    origin: 'top' , duration: 1500, distance: '60px', opacity : 0
+  })
+  ScrollReveal().reveal('.bottom',{
+    origin: 'bottom' , duration: 1000, distance: '60px',opacity : 0
+  })
+  ScrollReveal().reveal('.left',{
+    origin: 'left' , duration: 1000, distance: '60px', interval:100, opacity : 0
+  })
+  
+  ScrollReveal().reveal('.sag',{
+    origin: 'right' , duration: 1000, distance: '60px',  interval:100, opacity : 0
+  })
+  ScrollReveal().reveal('.sol',{
+    origin: 'left' , duration: 500, distance: '60px',  interval:100, opacity : 0
+  })
+  
+  
+  
